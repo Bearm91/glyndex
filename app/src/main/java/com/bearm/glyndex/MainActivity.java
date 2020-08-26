bfff8295bae77f5dff9b2d4fdcc99f19eb367465
Beatriz <bearm91@gmail.com>
2020-08-26 18:44:24 +0200

    Cat list fixes

--- app/src/main/java/com/bearm/glyndex/MainActivity.java
+++ app/src/main/java/com/bearm/glyndex/MainActivity.java
@@ -1,23 +1,29 @@
 package com.bearm.glyndex;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bearm.glyndex.activities.FoodActivity;
+import com.bearm.glyndex.adapters.CategoryAdapter;
 import com.bearm.glyndex.models.Category;
 import com.bearm.glyndex.repositories.CategoryRepository;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     CategoryAdapter adapter;
+    List<Category> categoryList;
+    RecyclerView rv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,38 +32,33 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        RecyclerView rv = (RecyclerView) findViewById(R.id.rv);
+        rv = (RecyclerView) findViewById(R.id.rv);
+        loadCategoryList();
 
-        CategoryRepository categoryRepository = new CategoryRepository(getApplication());
-        List<Category> categoryList = categoryRepository.getCategoryList();
 
-        //List<Category> categoryList = new ArrayList<>();
+    }
+
+    private void loadCategoryList() {
+
+
+        categoryList = getCategoryList();
 
         RecyclerView.LayoutManager layoutManager = new GridLayoutManager(this, 2);
-        adapter = new CategoryAdapter(this, categoryList);
+        adapter = new CategoryAdapter(this, categoryList, new CategoryAdapter.ItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+                goToFoodScreen(position);
+            }
+        });
+
         rv.setLayoutManager(layoutManager);
         rv.setAdapter(adapter);
 
     }
 
-    private List<Category> getExampleCategoryList() {
-        List<Category> categoryList = new ArrayList<>();
-        Category cat1 = new Category();
-        cat1.setId(1);
-        cat1.setName("Lácteos");
-        cat1.setIconName("ic_milk");
-        Category cat2 = new Category();
-        cat2.setId(2);
-        cat2.setName("Harina y cereales");
-        cat2.setIconName("ic_bread");
-        Category cat3 = new Category();
-        cat3.setId(3);
-        cat3.setName("Legumbres");
-        cat3.setIconName("ic_greenbeans");
-        categoryList.add(cat1);
-        categoryList.add(cat2);
-        categoryList.add(cat3);
-        return categoryList;
+    private List<Category> getCategoryList() {
+        CategoryRepository categoryRepository = new CategoryRepository(getApplication());
+        return categoryRepository.getCategoryList();
     }
 
     @Override
@@ -80,5 +81,15 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    private void goToFoodScreen(int position) {
+        Category cat = categoryList.get(position);
+        Log.e("Cat", String.valueOf(cat.toString()));
+        Intent foodIntent = new Intent(this, FoodActivity.class);
+        foodIntent.putExtra("CategoryId", cat.getId());
+        foodIntent.putExtra("CategoryName", cat.getName());
+        startActivity(foodIntent);
     }
 }
---
+++ app/src/main/java/com/bearm/glyndex/adapters/CategoryAdapter.java
@@ -0,0 +1,97 @@
+package com.bearm.glyndex.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bearm.glyndex.R;
+import com.bearm.glyndex.models.Category;
+
+import java.util.List;
+
+public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.ViewHolder> {
+
+    private List<Category> mData;
+    private LayoutInflater mInflater;
+    private ItemClickListener mClickListener;
+    private Context context;
+
+    // data is passed into the constructor
+    public CategoryAdapter(Context context, List<Category> data, ItemClickListener mClickListener) {
+        this.mInflater = LayoutInflater.from(context);
+        this.mData = data;
+        this.context = context;
+        this.mClickListener = mClickListener;
+    }
+
+    // inflates the cell layout from xml when needed
+    @Override
+    @NonNull
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        final View view = mInflater.inflate(R.layout.category_list_item, parent, false);
+
+        return new ViewHolder(view);
+    }
+
+    // binds the data to the TextView in each cell
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
+        Category currentCategory = mData.get(position);
+        holder.myTextView.setText(currentCategory.getName());
+        String iconName = currentCategory.getIconName();
+        if (iconName != null) {
+            int resourceIdImage = context.getResources().getIdentifier(iconName, "drawable",
+                    context.getPackageName());
+            //use this id to set the image anywhere
+            holder.myImageView.setImageResource(resourceIdImage);
+        }
+    }
+
+
+    // total number of cells
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+
+    // stores and recycles views as they are scrolled off screen
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView myTextView;
+        ImageView myImageView;
+        CardView myCardView;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            myTextView = itemView.findViewById(R.id.tv_cat_name);
+            myImageView = itemView.findViewById(R.id.iv_cat_icon);
+            myCardView = itemView.findViewById(R.id.cv_item);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (mClickListener != null) mClickListener.onItemClick(v, getAdapterPosition());
+                }
+            });
+
+        }
+
+    }
+
+    // convenience method for getting data at click position
+    Category getItem(int id) {
+        return mData.get(id);
+    }
+
+
+    // parent activity will implement this method to respond to click events
+    public interface ItemClickListener {
+        void onItemClick(View view, int position);
+    }
+}
