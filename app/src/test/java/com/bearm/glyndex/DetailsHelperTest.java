c5e673d28c4cf342c08b4c19855d634eb8ec2e85
Bea <bearm91@gmail.com>
2020-12-28 20:34:29 +0100

    DetailsHelper Testing

---
+++ app/src/test/java/com/bearm/glyndex/DetailsHelperTest.java
@@ -0,0 +1,38 @@
+package com.bearm.glyndex;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DetailsHelperTest {
+
+    private static String GREEN_ARROW = "ic_up_green_arrow";
+    private static String YELLOW_ARROW = "ic_up_yellow_arrow";
+    private static String RED_ARROW = "ic_up_red_arrow";
+    private static String BLUE_ARROW = "ic_up_blue_arrow";
+
+    @Test
+    public void getIGIconName_Red() {
+        assertEquals(DetailsHelper.getIGIconName(80), RED_ARROW);
+    }
+
+    @Test
+    public void getIGIconName_Yellow() {
+        assertEquals(DetailsHelper.getIGIconName(60), YELLOW_ARROW);
+    }
+
+    @Test
+    public void getIGIconName_Green() {
+        assertEquals(DetailsHelper.getIGIconName(50), GREEN_ARROW);
+    }
+
+    @Test
+    public void getIGIconName_Blue() {
+        assertEquals(DetailsHelper.getIGIconName(null), BLUE_ARROW);
+    }
+
+    @Test
+    public void getIGIconName_Green_zero() {
+        assertEquals(DetailsHelper.getIGIconName(0), GREEN_ARROW);
+    }
+}
