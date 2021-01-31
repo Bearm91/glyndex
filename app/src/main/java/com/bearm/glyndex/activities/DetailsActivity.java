8e94c5a7855808117f80315228256b2600394406
Beatriz <bearm91@gmail.com>
2021-01-31 15:18:34 +0100

    Remove info menu from Details screen

--- app/src/main/java/com/bearm/glyndex/activities/DetailsActivity.java
+++ app/src/main/java/com/bearm/glyndex/activities/DetailsActivity.java
@@ -146,31 +146,4 @@ public class DetailsActivity extends AppCompatActivity {
         onBackPressed();
         return true;
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_details, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        if (id == R.id.action_GI) {
-            goToInfoScreen();
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    private void goToInfoScreen(){
-        Intent infoIntent = new Intent(this, InfoActivity.class);
-        startActivity(infoIntent);
-    }
 }
